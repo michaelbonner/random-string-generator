30563a4f6e4cfc67b3cf30229b6dca3475bf761c
Michael Bonner <michaelwbonner@gmail.com>
2022-03-02 16:52:29 -0700

    return error if not a number used for generator

--- src/routes/[stringLength].ts
+++ src/routes/[stringLength].ts
@@ -1,7 +1,14 @@
 import { generateRandomString } from '../functions/generateRandomString';
 
 export async function get({ params }) {
+	const stringLength = parseInt(params.stringLength);
+	if (stringLength) {
+		return {
+			body: generateRandomString(stringLength || 32)
+		};
+	}
+
 	return {
-		body: generateRandomString(params.stringLength || 32)
+		status: 400
 	};
 }
