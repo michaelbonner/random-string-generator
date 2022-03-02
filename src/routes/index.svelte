60f89638e297dc039f335ae249b10f8350c1160a
Michael Bonner <michaelwbonner@gmail.com>
2022-03-02 12:15:54 -0700

    little styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,55 +12,59 @@
 	let string = generateString(stringlength);
 </script>
 
-<h1 class="text-3xl font-bold underline">Random String Generator</h1>
-<form class="mt-8">
-	<label for="stringlength">String Length</label>
-	<input
-		type="number"
-		id="stringlength"
-		name="stringlength"
-		bind:value={stringlength}
-		on:keyup={() => (string = generateString(stringlength))}
-		min="1"
-		max="1024"
-	/>
-</form>
-<button
-	class="font-mono my-4 py-4 px-8 border-2 border-dashed bg-gray-50 max-w-lg break-all text-left"
-	on:click={(e) => {
-		if (window.getSelection) {
-			const range = document.createRange();
-			range.selectNode(e.currentTarget);
-			window.getSelection().removeAllRanges();
-			window.getSelection().addRange(range);
-		}
-	}}
-	type="button"
->
-	{string}
-</button>
+<div class="grid gap-y-8">
+	<h1 class="text-3xl font-bold underline">Random String Generator</h1>
+	<form>
+		<label for="stringlength">String Length</label>
+		<input
+			type="number"
+			id="stringlength"
+			name="stringlength"
+			bind:value={stringlength}
+			on:keyup={() => (string = generateString(stringlength))}
+			min="1"
+			max="1024"
+		/>
+	</form>
+	<div>
+		<button
+			class="font-mono my-4 py-4 px-8 border-2 border-dashed bg-gray-50 max-w-lg break-all text-left"
+			on:click={(e) => {
+				if (window.getSelection) {
+					const range = document.createRange();
+					range.selectNode(e.currentTarget);
+					window.getSelection().removeAllRanges();
+					window.getSelection().addRange(range);
+				}
+			}}
+			type="button"
+		>
+			{string}
+		</button>
 
-<div>
-	<button
-		on:click={() => (string = generateString(stringlength))}
-		type="button"
-		class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-	>
-		Regenerate
-	</button>
-</div>
+		<div>
+			<button
+				on:click={() => (string = generateString(stringlength))}
+				type="button"
+				class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+			>
+				Regenerate
+			</button>
+		</div>
+	</div>
 
-{#if stringlength > 1024}
-	<p class="py-2 px-4 bg-red-200 border border-red-600 text-red-800 max-w-lg">
-		Max string length is 1024, you provided a length of {stringlength}
-	</p>
-{/if}
+	{#if stringlength > 1024}
+		<p class="py-2 px-4 bg-red-200 border border-red-600 text-red-800 max-w-lg">
+			Max string length is 1024, you provided a length of {stringlength}
+		</p>
+	{/if}
 
-<div class="prose bg-gray-50 mt-12 py-4 px-8 max-w-lg border border-gray-200">
-	<p>
-		Heads up:<br />
-		You can use
-		<a href="/?stringlength={stringlength}">?stringlength=<strong>{stringlength}</strong></a> in the
-		URL to set the string length.
-	</p>
+	<div class="prose bg-gray-50 py-4 px-8 max-w-lg border border-gray-200">
+		<p>
+			Heads up:<br />
+			You can use
+			<a href="/?stringlength={stringlength}">?stringlength=<strong>{stringlength}</strong></a> in the
+			URL to set the string length.
+		</p>
+	</div>
 </div>
